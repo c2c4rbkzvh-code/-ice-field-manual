fc990cc15aa89450b9a4fbf6f079be8a570ff3ce
Marcus och ICE <c2c4rbkzvh@privaterelay.appleid.com>
2026-08-09 16:10:18 +0200

    Add files via upload
    Field Manual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "field-manual-1-0-rc1-daily-use";
+const CACHE = "field-manual-1-0-rc2-rescue-roadmap";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./ice-photo.jpg","./ilo-photo.jpg","./icon-180.png","./icon-192.png","./icon-512.png","./ice-app-icon.png","./ice-hero-dark.jpg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
